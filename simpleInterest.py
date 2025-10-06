983a5197779907a2defcf0e46769fde9c397c91b
SayedMohammedYahiyaKhadri <sayedyahiya18@gmail.com>
2025-10-06 15:27:51 +0530

    Update simpleInterest.py

--- simpleInterest.py
+++ simpleInterest.py
@@ -1,5 +1,5 @@
-p=float(input("Enter Principle amount:")
-r=float(input("Enter rate of interest:")
+p=float(input("Enter Principle amount:"))
+r=float(input("Enter rate of interest:"))
 t=int(input("Enter time (in years):")
 si=(p*t*r)/100
 print("Simple interest is:",si)
